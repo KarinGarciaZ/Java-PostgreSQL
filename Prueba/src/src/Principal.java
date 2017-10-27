21317893b06bb83c00ab152ccf174f7b6bc8e9ce
KarinGarciaZ <oscarin962011@hotmail.com>
2017-10-26 21:25:01 -0500

    First aproach.

---
+++ Prueba/src/src/Principal.java
@@ -0,0 +1,252 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] argv) {
+	try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your PostgreSQL JDBC Driver? Include in your library path!");
+            e.printStackTrace();
+            return;
+	}
+        System.out.println("PostgreSQL JDBC Driver Registered!");   
+	Connection connection = null;
+        try {
+            Scanner leer = new Scanner(System.in);
+            int opc, opc2;
+            connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/Localidades", "postgres","Bankai123");
+            do{
+                System.out.println("\n-------------------------------");
+                System.out.println("1.- Ciudades.");
+                System.out.println("2.- Municipios.");
+                System.out.println("3.- Estados.");
+                System.out.println("4.- Salir.");                
+                System.out.print("Ingresar opcion:  ");
+                opc = leer.nextInt();
+                if (opc > 0 && opc < 4){
+                    System.out.println("\n1.- Insertar.");
+                    System.out.println("2.- Consultar.");
+                    System.out.println("3.- Eliminar.");
+                    System.out.println("4.- Modificar.");
+                    System.out.print("Ingresar opcion:  ");
+                    opc2 = leer.nextInt();
+                    
+                    switch (opc){
+                        case 1:
+                        {
+                            switch (opc2){
+                                case 1:
+                                {
+                                    System.out.print("\n1.- Id a insertar: ");
+                                    int id = leer.nextInt();
+                                    
+                                    leer.nextLine();
+                                    System.out.print("2.- Nombre a insertar: ");
+                                    String nom = leer.nextLine();
+                                    PreparedStatement stmt = connection.prepareStatement("INSERT INTO public.\"Ciudades\"(id, nombre) VALUES (" + id + ",'" + nom + "')");
+                                    stmt.executeUpdate();
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    System.out.println("\n--------CONSULTA GENERAL---------");
+                                    PreparedStatement stmt = connection.prepareStatement("SELECT id, nombre FROM public.\"Ciudades\"");
+                                    ResultSet Rs = stmt.executeQuery();
+                                    while(Rs.next())
+                                        System.out.println(Rs.getInt(1) + "     " + Rs.getString(2));   
+                                    break;
+                                }
+                                case 3:
+                                {
+                                    System.out.println("\n--------CONSULTA GENERAL---------");
+                                    PreparedStatement stmt = connection.prepareStatement("SELECT id, nombre FROM public.\"Ciudades\"");
+                                    ResultSet Rs = stmt.executeQuery();
+                                    while(Rs.next())
+                                        System.out.println(Rs.getInt(1) + "     " + Rs.getString(2));
+                                    
+                                    System.out.print("Id de elemento a eliminar: ");
+                                    int eli = leer.nextInt();
+                                    stmt = connection.prepareStatement("DELETE FROM public.\"Ciudades\" WHERE id = " + eli + "");
+                                    stmt.executeUpdate();
+                                    
+                                    break;
+                                }
+                                case 4:
+                                {
+                                    System.out.println("\n--------CONSULTA GENERAL---------");
+                                    PreparedStatement stmt = connection.prepareStatement("SELECT id, nombre FROM public.\"Ciudades\"");
+                                    ResultSet Rs = stmt.executeQuery();
+                                    while(Rs.next())
+                                        System.out.println(Rs.getInt(1) + "     " + Rs.getString(2));
+                                    
+                                    System.out.print("Id de elemento a modificar: ");
+                                    int mod = leer.nextInt();   
+                                    
+                                    System.out.print("1.- Id a insertar: ");
+                                    int id = leer.nextInt();
+                                                                        
+                                    leer.nextLine();
+                                    System.out.print("2.- Nombre a insertar: ");
+                                    String nom = leer.nextLine();
+                                    
+                                    stmt = connection.prepareStatement("UPDATE public.\"Ciudades\" SET id=" + id + ", nombre='" + nom + "' WHERE id = " + mod + "");
+                                    stmt.executeUpdate();
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                        case 2:
+                        {
+                            switch (opc2){
+                                case 1:
+                                {
+                                    System.out.print("\n1.- Id a insertar: ");
+                                    int id = leer.nextInt();
+                                    
+                                    leer.nextLine();
+                                    System.out.print("2.- Nombre a insertar: ");
+                                    String nom = leer.nextLine();
+                                    PreparedStatement stmt = connection.prepareStatement("INSERT INTO public.\"Municipios\"(id, nombre) VALUES (" + id + ",'" + nom + "')");
+                                    stmt.executeUpdate();
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    System.out.println("\n--------CONSULTA GENERAL---------");
+                                    PreparedStatement stmt = connection.prepareStatement("SELECT id, nombre FROM public.\"Municipios\"");
+                                    ResultSet Rs = stmt.executeQuery();
+                                    while(Rs.next())
+                                        System.out.println(Rs.getInt(1) + "     " + Rs.getString(2));   
+                                    break;
+                                }
+                                case 3:
+                                {
+                                    System.out.println("\n--------CONSULTA GENERAL---------");
+                                    PreparedStatement stmt = connection.prepareStatement("SELECT id, nombre FROM public.\"Municipios\"");
+                                    ResultSet Rs = stmt.executeQuery();
+                                    while(Rs.next())
+                                        System.out.println(Rs.getInt(1) + "     " + Rs.getString(2));
+                                    
+                                    System.out.print("Id de elemento a eliminar: ");
+                                    int eli = leer.nextInt();
+                                    stmt = connection.prepareStatement("DELETE FROM public.\"Municipios\" WHERE id = " + eli + "");
+                                    stmt.executeUpdate();
+                                    
+                                    break;
+                                }
+                                case 4:
+                                {
+                                    System.out.println("\n--------CONSULTA GENERAL---------");
+                                    PreparedStatement stmt = connection.prepareStatement("SELECT id, nombre FROM public.\"Municipios\"");
+                                    ResultSet Rs = stmt.executeQuery();
+                                    while(Rs.next())
+                                        System.out.println(Rs.getInt(1) + "     " + Rs.getString(2));
+                                    
+                                    System.out.print("Id de elemento a modificar: ");
+                                    int mod = leer.nextInt();   
+                                    
+                                    System.out.print("1.- Id a insertar: ");
+                                    int id = leer.nextInt();
+                                                                        
+                                    leer.nextLine();
+                                    System.out.print("2.- Nombre a insertar: ");
+                                    String nom = leer.nextLine();
+                                    
+                                    stmt = connection.prepareStatement("UPDATE public.\"Municipios\" SET id=" + id + ", nombre='" + nom + "' WHERE id = " + mod + "");
+                                    stmt.executeUpdate();
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                        case 3:
+                        {
+                            switch (opc2){
+                                case 1:
+                                {
+                                    System.out.print("\n1.- Id a insertar: ");
+                                    int id = leer.nextInt();
+                                    
+                                    leer.nextLine();
+                                    System.out.print("2.- Nombre a insertar: ");
+                                    String nom = leer.nextLine();
+                                    PreparedStatement stmt = connection.prepareStatement("INSERT INTO public.\"Estados\"(id, nombre) VALUES (" + id + ",'" + nom + "')");
+                                    stmt.executeUpdate();
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    System.out.println("\n--------CONSULTA GENERAL---------");
+                                    PreparedStatement stmt = connection.prepareStatement("SELECT id, nombre FROM public.\"Estados\"");
+                                    ResultSet Rs = stmt.executeQuery();
+                                    while(Rs.next())
+                                        System.out.println(Rs.getInt(1) + "     " + Rs.getString(2));   
+                                    break;
+                                }
+                                case 3:
+                                {
+                                    System.out.println("\n--------CONSULTA GENERAL---------");
+                                    PreparedStatement stmt = connection.prepareStatement("SELECT id, nombre FROM public.\"Estados\"");
+                                    ResultSet Rs = stmt.executeQuery();
+                                    while(Rs.next())
+                                        System.out.println(Rs.getInt(1) + "     " + Rs.getString(2));
+                                    
+                                    System.out.print("Id de elemento a eliminar: ");
+                                    int eli = leer.nextInt();
+                                    stmt = connection.prepareStatement("DELETE FROM public.\"Estados\" WHERE id = " + eli + "");
+                                    stmt.executeUpdate();
+                                    
+                                    break;
+                                }
+                                case 4:
+                                {
+                                    System.out.println("\n--------CONSULTA GENERAL---------");
+                                    PreparedStatement stmt = connection.prepareStatement("SELECT id, nombre FROM public.\"Estados\"");
+                                    ResultSet Rs = stmt.executeQuery();
+                                    while(Rs.next())
+                                        System.out.println(Rs.getInt(1) + "     " + Rs.getString(2));
+                                    
+                                    System.out.print("Id de elemento a modificar: ");
+                                    int mod = leer.nextInt();   
+                                    
+                                    System.out.print("1.- Id a insertar: ");
+                                    int id = leer.nextInt();
+                                                                        
+                                    leer.nextLine();
+                                    System.out.print("2.- Nombre a insertar: ");
+                                    String nom = leer.nextLine();
+                                    
+                                    stmt = connection.prepareStatement("UPDATE public.\"Estados\" SET id=" + id + ", nombre='" + nom + "' WHERE id = " + mod + "");
+                                    stmt.executeUpdate();
+                                    break;
+                                }
+                            }
+                            break;
+                        }                        
+                    }
+                }               
+                                                
+            }while(opc != 4);
+            
+            
+            
+        }catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }             
+    } 
+}
